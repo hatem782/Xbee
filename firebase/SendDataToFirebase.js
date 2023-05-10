75b44006b6aba587076b50dbe14a20b3403720f9
Gassem-ilhem <gassem.ilhem@gmail.com>
2023-05-11 01:56:45 +0200

    add notification

--- firebase/SendDataToFirebase.js
+++ firebase/SendDataToFirebase.js
@@ -1,4 +1,12 @@
 const { database } = require("./config");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+let can_send = true;
+let sended_date = null;
 
 // Create a new item in the database
 const createTemp = (value) => {
@@ -9,6 +17,31 @@ const createTemp = (value) => {
   }/${date.getFullYear()}`;
   let time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
   newItemRef.set({ value, date: actual_date, time });
+  // send notification
+
+  if (
+    value > 35 &&
+    (sended_date === null || date.getTime() - sended_date.getTime() > 60 * 1000)
+  ) {
+    sended_date = new Date();
+    const message = {
+      notification: {
+        title: "Over Temp",
+        body: `Temp is now over than ${value}`,
+      },
+      topic: "Temp_Notif",
+    };
+
+    admin
+      .messaging()
+      .send(message)
+      .then((response) => {
+        console.log("Successfully sent message:", response);
+      })
+      .catch((error) => {
+        console.error("Error sending message:", error);
+      });
+  }
 };
 
 module.exports = { createTemp };
